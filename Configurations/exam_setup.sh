8da0f81d49cd68fa843e90ab5c5fbf7e2d5f332b
bznerd <guyinthechair631@gmail.com>
2020-10-15 12:51:14 -0400

    Moved custom contents of old repo to standalone repo

---
+++ Configurations/exam_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export KUBE_EDITOR="vim"
+alias k=kubectl
+minikube start --driver=docker
---
+++ Quick_Reference/bash_quick_reference.sh
@@ -0,0 +1,6 @@
+# don't forget man pages
+man whatever_command
+
+# wget example: download to:location from:location
+wget "-O" "/usr/shared/app/config.json" https://raw.githubusercontent.com/bmuschko/ckad-crash-course/master/exercises/07-creating-init-container/app/config/config.json
+
