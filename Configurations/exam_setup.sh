26ae7d270633bc1b4b7200ddad34c4c8768cd8ee
Nishan Wijesinghe <nishan@parigan.com>
2020-10-22 14:11:25 -0400

    Adding practice machine setup after working with Dante on his new mac. Cleaned up other reference material.

--- Configurations/exam_setup.sh
+++ Configurations/exam_setup.sh
@@ -3,3 +3,10 @@
 export KUBE_EDITOR="vim"
 alias k=kubectl
 minikube start --driver=docker
+
+# Other time-savers:
+# Ref: https://github.com/bmuschko/ckad-prep/blob/master/8-bonus.md#bonus-exercises
+
+k config set-context --current --namespace=<insert-namespace-name-here>
+# Validate it
+k config view --minify | grep namespace:
